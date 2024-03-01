cf90af4406256360c8764f6b8466fda0acc5755a
David B. Wright <david.b.wright@afs.com>
2024-03-01 14:21:05 -0600

    Squashed commit of the following:
    commit 34db571bc949a53af5f3a346c00baa10cc017e1d
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Fri Mar 1 13:42:43 2024 -0600
    
        Remove CLI project
    
    commit 744c6e2dadf2f60726b23ae63897c2f875b6752d
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Wed Feb 28 11:05:06 2024 -0600
    
        Make print thread safe
    
    commit a3ef36f5b0a49a127bffc1cf79d30830d1219026
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Wed Feb 28 11:04:38 2024 -0600
    
        More info in error
    
    commit d801c758f64f14c94f007e286ebd64b4729d1dc5
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Wed Feb 28 09:59:19 2024 -0600
    
        Format library
    
    commit 9d89d3a3d8fdc7e6f5ed9f2ade1602ff29d978e0
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Tue Feb 27 22:11:50 2024 -0600
    
        Filter termintated instances
    
    commit 16bb752daa1b8b24692a1b51e843ec27faadd7be
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Tue Feb 27 20:47:12 2024 -0600
    
        Clean up test folder
    
    commit 3e9022eefc6539071c85366d824bb111c63901aa
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Tue Feb 27 20:46:39 2024 -0600
    
        Finish testing script
    
    commit ca6bc2d2be5cbf491eafaaa2c8ac12c943092d7f
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Tue Feb 27 18:40:06 2024 -0600
    
        Format
    
    commit da92274da8e94ae0da3f60395338955b84ee9c5f
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Tue Feb 27 18:31:21 2024 -0600
    
        Change error name
    
    commit 12cd800547c5deea339416a141c64811230aaec1
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Tue Feb 27 18:30:00 2024 -0600
    
        Return instances from functions
    
    commit 203c46423d64d04269640ecfdb07df8c04716762
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Tue Feb 27 17:48:32 2024 -0600
    
        Add custom errors
    
    commit 7cd7b2e2ce9905d9ca8d1562c09498ce75a37e65
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Mon Feb 26 17:43:09 2024 -0600
    
        Fix ami tagging and wait for instances to fully start
    
    commit 18b671afa74d90bdc28cdd7d48911d305f8b0798
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Mon Feb 26 17:39:05 2024 -0600
    
        Set up testing
    
    commit 7d84eb2b3544182e8d23b15967dc005903d07b5e
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Mon Feb 26 16:51:16 2024 -0600
    
        Use result type
    
    commit 3372acdc9da54eb081652dc8fd3074aac7bb8dae
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Mon Feb 26 14:46:33 2024 -0600
    
        Add packages
    
    commit 182598ada8c9deac8b337a775c1ec35536817523
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Mon Feb 26 13:55:41 2024 -0600
    
        Add testing
    
    commit 9a844f03ffdcfadca42937f1efda78151595fdba
    Author: captainsalt <dontbothertheking@gmail.com>
    Date:   Mon Feb 26 13:47:29 2024 -0600
    
        Change folder names
    
    commit ac1039e7e5e954b07d5666e3c32d36aeb3478007
    Author: captainsalt <dontbothertheking@gmail.com>
    Date:   Mon Feb 26 13:38:02 2024 -0600
    
        Add sln
    
    commit 4e63e54af2335f0f907fbe55e117cbb88a0dd9ff
    Author: captainsalt <dontbothertheking@gmail.com>
    Date:   Mon Feb 26 13:37:50 2024 -0600
    
        Add sln
    
    commit 8021d3e76b2312eb24d94d3c67b19b477cf9e456
    Author: captainsalt <dontbothertheking@gmail.com>
    Date:   Mon Feb 26 13:36:45 2024 -0600
    
        Change folder names
    
    commit 8d6dee573eec55568863a40b91c5f095ad400a73
    Author: captainsalt <dontbothertheking@gmail.com>
    Date:   Mon Feb 26 13:34:51 2024 -0600
    
        Split project into files
    
    commit c10aabd2ad1981a4fc3830f3e86acbcd53303b49
    Author: captainsalt <dontbothertheking@gmail.com>
    Date:   Sun Feb 25 23:39:58 2024 -0600
    
        Testing

---
+++ WorkScripts.Testing/Program.fs
@@ -0,0 +1,64 @@
+﻿open Amazon.Runtime.CredentialManagement
+open Amazon.Runtime
+open Amazon.EC2
+open WorkScripts.Library.EC2
+open System
+
+let print (input: string) = System.Console.WriteLine(input)
+
+let snapshotWorkflow ec2Client instanceNames = 
+    let ( >>= ) computation fn = 
+        async {
+            let! result = computation
+
+            match result with 
+            | Ok r -> return! fn r
+            | Error s -> return Error s
+        }
+
+    let workflow instanceName = 
+        getInstanceByName ec2Client instanceName
+        >>= fun instance ->
+            print $"Stopping {instance.InstanceId}"
+            stopInstance ec2Client instance
+        >>= fun instance ->
+            let rng = Random()
+            let request = {
+                amiName = $"DELETE ME {rng.Next()}"
+                description = "DELETE ME"
+                tags = ["Name", "DELETE ME"]
+                instance = null
+            }
+            print $"""Creating ami for {instance.InstanceId}"""
+            createAmi ec2Client { request with instance = instance }
+        >>= fun instance -> 
+            print $"Starting {instance.InstanceId}"
+            startInstance ec2Client instance
+
+    instanceNames 
+    |> Seq.map (fun instanceName -> workflow instanceName)
+    |> Async.Parallel
+
+let createEC2ClientWithProfile profileName =
+    let chain = CredentialProfileStoreChain()
+    let mutable credentials = Unchecked.defaultof<AWSCredentials>
+    if chain.TryGetAWSCredentials(profileName, &credentials) then
+        Ok (new AmazonEC2Client(credentials, Amazon.RegionEndpoint.USGovCloudEast1))
+    else 
+        Error $"Failed to create EC2 client. Profile not found: {profileName}"
+
+// Example usage
+let profileName = "lab"
+let ec2ClientResult = createEC2ClientWithProfile profileName
+
+match ec2ClientResult with
+| Ok ec2Client ->
+    try
+        let names = [for i in 0..100 -> $"dw-instance-{i}"] @ ["dlg-OSC Services Host"]
+        let results = snapshotWorkflow ec2Client names |> Async.RunSynchronously
+        printfn "Results: %A" results
+    with
+    | ex ->
+        eprintf "Unexpected error: %s" ex.Message
+| Error errorMsg ->
+    printfn "Error creating client: %s" errorMsg
