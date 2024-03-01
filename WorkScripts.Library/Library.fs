8fad2271a37651c56f93d1c832f365badbbd3bb9
captainsalt <dontbothertheking@gmail.com>
2024-03-01 14:21:05 -0600

    Squashed commit of the following:
    commit 34db571bc949a53af5f3a346c00baa10cc017e1d
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Fri Mar 1 13:42:43 2024 -0600
    
        Remove CLI project
    
    commit 744c6e2dadf2f60726b23ae63897c2f875b6752d
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Wed Feb 28 11:05:06 2024 -0600
    
        Make print thread safe
    
    commit a3ef36f5b0a49a127bffc1cf79d30830d1219026
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Wed Feb 28 11:04:38 2024 -0600
    
        More info in error
    
    commit d801c758f64f14c94f007e286ebd64b4729d1dc5
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Wed Feb 28 09:59:19 2024 -0600
    
        Format library
    
    commit 9d89d3a3d8fdc7e6f5ed9f2ade1602ff29d978e0
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Tue Feb 27 22:11:50 2024 -0600
    
        Filter termintated instances
    
    commit 16bb752daa1b8b24692a1b51e843ec27faadd7be
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Tue Feb 27 20:47:12 2024 -0600
    
        Clean up test folder
    
    commit 3e9022eefc6539071c85366d824bb111c63901aa
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Tue Feb 27 20:46:39 2024 -0600
    
        Finish testing script
    
    commit ca6bc2d2be5cbf491eafaaa2c8ac12c943092d7f
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Tue Feb 27 18:40:06 2024 -0600
    
        Format
    
    commit da92274da8e94ae0da3f60395338955b84ee9c5f
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Tue Feb 27 18:31:21 2024 -0600
    
        Change error name
    
    commit 12cd800547c5deea339416a141c64811230aaec1
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Tue Feb 27 18:30:00 2024 -0600
    
        Return instances from functions
    
    commit 203c46423d64d04269640ecfdb07df8c04716762
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Tue Feb 27 17:48:32 2024 -0600
    
        Add custom errors
    
    commit 7cd7b2e2ce9905d9ca8d1562c09498ce75a37e65
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Mon Feb 26 17:43:09 2024 -0600
    
        Fix ami tagging and wait for instances to fully start
    
    commit 18b671afa74d90bdc28cdd7d48911d305f8b0798
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Mon Feb 26 17:39:05 2024 -0600
    
        Set up testing
    
    commit 7d84eb2b3544182e8d23b15967dc005903d07b5e
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Mon Feb 26 16:51:16 2024 -0600
    
        Use result type
    
    commit 3372acdc9da54eb081652dc8fd3074aac7bb8dae
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Mon Feb 26 14:46:33 2024 -0600
    
        Add packages
    
    commit 182598ada8c9deac8b337a775c1ec35536817523
    Author: David B. Wright <david.b.wright@afs.com>
    Date:   Mon Feb 26 13:55:41 2024 -0600
    
        Add testing
    
    commit 9a844f03ffdcfadca42937f1efda78151595fdba
    Author: captainsalt <dontbothertheking@gmail.com>
    Date:   Mon Feb 26 13:47:29 2024 -0600
    
        Change folder names
    
    commit ac1039e7e5e954b07d5666e3c32d36aeb3478007
    Author: captainsalt <dontbothertheking@gmail.com>
    Date:   Mon Feb 26 13:38:02 2024 -0600
    
        Add sln
    
    commit 4e63e54af2335f0f907fbe55e117cbb88a0dd9ff
    Author: captainsalt <dontbothertheking@gmail.com>
    Date:   Mon Feb 26 13:37:50 2024 -0600
    
        Add sln
    
    commit 8021d3e76b2312eb24d94d3c67b19b477cf9e456
    Author: captainsalt <dontbothertheking@gmail.com>
    Date:   Mon Feb 26 13:36:45 2024 -0600
    
        Change folder names
    
    commit 8d6dee573eec55568863a40b91c5f095ad400a73
    Author: captainsalt <dontbothertheking@gmail.com>
    Date:   Mon Feb 26 13:34:51 2024 -0600
    
        Split project into files
    
    commit c10aabd2ad1981a4fc3830f3e86acbcd53303b49
    Author: captainsalt <dontbothertheking@gmail.com>
    Date:   Sun Feb 25 23:39:58 2024 -0600
    
        Testing

---
+++ WorkScripts.Library/Library.fs
@@ -0,0 +1,158 @@
+﻿namespace WorkScripts.Library
+
+module EC2 =
+    open Amazon.EC2
+    open System.Collections.Generic
+    open Amazon.EC2.Model
+
+    type AmiRequest =
+        { amiName: string
+          description: string
+          tags: (string * string) seq
+          instance: Instance }
+
+    type Error =
+        | InstanceNotFound of string
+        | ImageNotFound of string
+        | InstanceTerminated of string
+        | MultipleInstancesFound of string
+        | ErrorStoppingInstance of string
+        | ErrorStartingInstance of string
+        | ErrorCreatingImage of string
+
+    let private tagsFromTuple (tags: (string * string) seq) =
+        tags |> Seq.map (fun (name, value) -> new Tag(name, value))
+
+    let private createDescribeInstancesRequest filterName filterValue =
+        new DescribeInstancesRequest(
+            Filters = new List<Filter>([ new Filter(filterName, new List<string>([ filterValue ])) ])
+        )
+
+    let private extractInstances (response: DescribeInstancesResponse) =
+        response.Reservations |> Seq.collect _.Instances |> Seq.toList
+
+    let rec private waitForImage (ec2Client: AmazonEC2Client) (imageId: string) =
+        async {
+            let describeImageRequest =
+                DescribeImagesRequest(ImageIds = new List<string>([ imageId ]))
+
+            let! imageResult = ec2Client.DescribeImagesAsync(describeImageRequest) |> Async.AwaitTask
+            let invalidStates = [ ImageState.Error; ImageState.Failed; ImageState.Invalid ]
+
+            let image = imageResult.Images |> Seq.tryHead
+
+            match image with
+            | Some image when image.State.Value = ImageState.Available.Value -> return Ok image.ImageId
+            | Some image when invalidStates |> List.contains image.State ->
+                return Error(ErrorCreatingImage $"Error creating image for instance {image.SourceInstanceId}")
+            | Some image ->
+                do! Async.Sleep 3_000
+                return! waitForImage ec2Client image.ImageId
+            | None -> return Error(ImageNotFound $"Image {imageId} not found")
+        }
+
+    let getInstanceById (ec2Client: AmazonEC2Client) (instanceId: string) =
+        async {
+            let request = createDescribeInstancesRequest "instance-id" instanceId
+            let! response = ec2Client.DescribeInstancesAsync(request) |> Async.AwaitTask
+            let instances = extractInstances response
+
+            match instances with
+            | [ instance ] when instance.State.Name = InstanceStateName.Terminated ->
+                return Error(InstanceTerminated $"Instance '{instance.InstanceId}' has been terminated")
+            | [ instance ] -> return Ok instance
+            | _ -> return Error(InstanceNotFound $"Instance with id '{instanceId}' not found")
+        }
+
+    let getInstanceByName (ec2Client: AmazonEC2Client) (nameTag: string) =
+        async {
+            let request = createDescribeInstancesRequest "tag:Name" nameTag
+            let! response = ec2Client.DescribeInstancesAsync(request) |> Async.AwaitTask
+            let instances = extractInstances response
+
+            match instances with
+            | [ instance ] when instance.State.Name = InstanceStateName.Terminated ->
+                return Error(InstanceTerminated $"Instance with Name tag '{nameTag}' has been terminated")
+            | [ instance ] -> return Ok instance
+            | [] -> return Error(InstanceNotFound $"Instance with name '{nameTag}' not found")
+            | instances ->
+                let instanceIds =
+                    instances |> List.map _.InstanceId |> List.reduce (sprintf "%s, %s")
+
+                return
+                    Error(
+                        MultipleInstancesFound
+                            $"Multiple instances found with tag name '{nameTag}'. Their ids are: {instanceIds}"
+                    )
+        }
+
+    let stopInstance (ec2Client: AmazonEC2Client) (instance: Instance) =
+        async {
+            do!
+                ec2Client.StopInstancesAsync(StopInstancesRequest(InstanceIds = ResizeArray [ instance.InstanceId ]))
+                |> Async.AwaitTask
+                |> Async.Ignore
+
+            let rec waitUntilStopped () =
+                async {
+                    let! instanceResult = getInstanceById ec2Client instance.InstanceId
+
+                    match instanceResult with
+                    | Ok instance when instance.State.Name = InstanceStateName.Stopped -> return Ok instance
+                    | Ok _ ->
+                        do! Async.Sleep 2_500
+                        return! waitUntilStopped ()
+                    | Error s ->
+                        return
+                            Error(ErrorStoppingInstance $"Error while stopping instance '{instance.InstanceId}': {s}")
+                }
+
+            return! waitUntilStopped ()
+        }
+
+    let createAmi (ec2Client: AmazonEC2Client) (amiRequest: AmiRequest) =
+        async {
+            let tags = tagsFromTuple amiRequest.tags
+
+            let imageRequest =
+                new CreateImageRequest(
+                    InstanceId = amiRequest.instance.InstanceId,
+                    Name = amiRequest.amiName,
+                    TagSpecifications =
+                        new List<TagSpecification>(
+                            [ new TagSpecification(ResourceType = ResourceType.Image, Tags = new List<Tag>(tags))
+                              new TagSpecification(ResourceType = ResourceType.Snapshot, Tags = new List<Tag>(tags)) ]
+                        )
+                )
+
+            let! response = ec2Client.CreateImageAsync(imageRequest) |> Async.AwaitTask
+            let! imageResult = waitForImage ec2Client response.ImageId
+
+            match imageResult with
+            | Ok _ -> return Ok amiRequest.instance
+            | Error err -> return Error err
+        }
+
+    let startInstance (ec2Client: AmazonEC2Client) (instance: Instance) =
+        async {
+            do!
+                ec2Client.StartInstancesAsync(StartInstancesRequest(InstanceIds = ResizeArray [ instance.InstanceId ]))
+                |> Async.AwaitTask
+                |> Async.Ignore
+
+            let rec waitUntilStarted () =
+                async {
+                    let! instanceResult = getInstanceById ec2Client instance.InstanceId
+
+                    match instanceResult with
+                    | Ok instance when instance.State.Name = InstanceStateName.Running -> return Ok instance
+                    | Ok _ ->
+                        do! Async.Sleep 2_500
+                        return! waitUntilStarted ()
+                    | Error msg ->
+                        return
+                            Error(ErrorStartingInstance $"Error when starting instance '{instance.InstanceId}': {msg}")
+                }
+
+            return! waitUntilStarted ()
+        }
