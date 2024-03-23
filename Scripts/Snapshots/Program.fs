cfe1844cb6b90ad02c5cfad22665774341d38de0
David Wright <dontbothertheking@gmail.com>
2024-03-22 21:21:38 -0500

    Working on snapshots

--- Scripts/Snapshots/Program.fs
+++ Scripts/Snapshots/Program.fs
@@ -3,23 +3,42 @@ open WorkScripts.Library.EC2
 open System.IO
 open Workflow
 open Amazon.EC2
+open Amazon.EC2.Model
 open SnapshotArgs
 
 let locateInstance awsProfile instanceName = 
     let regions = Amazon.RegionEndpoint.EnumerableAllRegions
 
-    [ for region in regions do 
-        useLocalCredentials "lab" 
-        |> Result.map 
-            (fun creds -> 
-                let client = new AmazonEC2Client(creds, region)
-                getInstanceByName client instanceName
-            )
-    ]
+    match useLocalCredentials awsProfile with 
+    | Ok credentials ->
+        [ for region in regions ->
+            async {
+                let client = new AmazonEC2Client(credentials, region)
+                let! instanceResult = getInstanceByName client instanceName
+
+                match instanceResult with 
+                | Error (InstanceNotFound _) -> return None
+                | Error err -> return failwith $"Unexpected error {err}"
+                | Ok instance -> return Some (region.SystemName, instance)
+            }
+        ]
+        |> Async.Parallel
+        |> Async.RunSynchronously
+        |> Seq.choose id
+        |> Seq.exactlyOne
+    | Error err -> failwith err
 
 [<EntryPoint>]
 let main args = 
-    locateInstance "" ""
+    let parsedArgs = cliParser.Parse args
+    let awsProfile = parsedArgs.GetResult Profile
+    
+    // Implementation    
+    let instanceIds  = parsedArgs.GetResult Instance_Ids |> File.ReadAllLines
+    let instanceLocations = 
+        instanceIds 
+        |> Seq.map (locateInstance awsProfile)
+        |> Map.ofSeq
 
-    // printfn "%A" (cliParser.Parse args)
+    printfn "%A" (cliParser.Parse args)
     0
