67ebf9ab5cbbec70b42447926abe76f292fd5b49
captainsalt <dontbothertheking@gmail.com>
2024-03-25 13:34:11 -0500

    Add script to get a url of all the instance

---
+++ Scripts/Snapshots/InstanceUrl.fsx
@@ -0,0 +1,16 @@
+open System
+open System.IO
+
+let urls instanceNames = 
+    let formattedInstances = 
+        instanceNames
+        |> Seq.reduce (sprintf "%s,%s")
+
+    [ $"https://console.amazonaws-us-gov.com/ec2/home?region=us-gov-east-1#Instances:tag:Name={formattedInstances};v=3;$case=tags:true%%5C,client:false;$regex=tags:false%%5C,client:false"
+      $"https://console.amazonaws-us-gov.com/ec2/home?region=us-gov-west-1#Instances:tag:Name={formattedInstances};v=3;$case=tags:true%%5C,client:false;$regex=tags:false%%5C,client:false" ]
+
+File.ReadAllLines "instances.txt"
+|> Seq.map _.Trim()
+|> Seq.filter (String.IsNullOrWhiteSpace >> not)
+|> urls
+|> Seq.iter (printfn "%s")
