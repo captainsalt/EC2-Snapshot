54a8e0a6cf3f1bb5344609353b1e09b7e3eb28f2
captainsalt <dontbothertheking@gmail.com>
2024-04-07 16:10:29 -0500

    Make one liner

--- Scripts/Snapshots/InstanceUrl.fsx
+++ Scripts/Snapshots/InstanceUrl.fsx
@@ -2,9 +2,7 @@ open System
 open System.IO
 
 let urls instanceNames = 
-    let formattedInstances = 
-        instanceNames
-        |> Seq.reduce (sprintf "%s,%s")
+    let formattedInstances = instanceNames |> Seq.reduce (sprintf "%s,%s")
 
     [ $"https://console.amazonaws-us-gov.com/ec2/home?region=us-gov-east-1#Instances:tag:Name={formattedInstances};v=3;$case=tags:true%%5C,client:false;$regex=tags:false%%5C,client:false"
       $"https://console.amazonaws-us-gov.com/ec2/home?region=us-gov-west-1#Instances:tag:Name={formattedInstances};v=3;$case=tags:true%%5C,client:false;$regex=tags:false%%5C,client:false" ]
