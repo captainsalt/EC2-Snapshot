94d67a3b86645763ad126bed1e625a962b828252
captainsalt <dontbothertheking@gmail.com>
2024-04-07 16:13:44 -0500

    Update code

--- Scripts/Snapshots/InstanceUrl.fsx
+++ Scripts/Snapshots/InstanceUrl.fsx
@@ -4,8 +4,9 @@ open System.IO
 let toUrls instanceNames = 
     let formattedInstances = instanceNames |> Seq.reduce (sprintf "%s,%s")
 
-    [ $"https://console.amazonaws-us-gov.com/ec2/home?region=us-gov-east-1#Instances:tag:Name={formattedInstances};v=3;$case=tags:true%%5C,client:false;$regex=tags:false%%5C,client:false"
-      $"https://console.amazonaws-us-gov.com/ec2/home?region=us-gov-west-1#Instances:tag:Name={formattedInstances};v=3;$case=tags:true%%5C,client:false;$regex=tags:false%%5C,client:false" ]
+    [ for region in [ "east"; "west" ] -> 
+        $"https://console.amazonaws-us-gov.com/ec2/home?region=us-gov-{region}-1#Instances:tag:Name={formattedInstances};v=3;$case=tags:true%%5C,client:false;$regex=tags:false%%5C,client:false" 
+        ]
 
 File.ReadAllLines "instances.txt"
 |> Seq.map _.Trim()
