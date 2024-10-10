f179262cdef20220a62a133ce7ece3a44bc79af5
captainsalt <dontbothertheking@gmail.com>
2024-10-09 18:43:51 -0700

    Move project to root path

--- addprojects.sh
+++
@@ -1,7 +0,0 @@
-SCRIPT=$(realpath "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-
-(
-    cd $SCRIPTPATH
-    find . -name *.fsproj | xargs dotnet sln add
-)
--- pack.sh
+++
@@ -1,8 +0,0 @@
-SCRIPT=$(realpath "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-
-(
-    cd $SCRIPTPATH
-    cd WorkScripts.Library
-    dotnet pack -o '../NugetLocal'
-)
